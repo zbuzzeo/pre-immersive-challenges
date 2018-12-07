c3354f23dc315480700d65b8f6e766cea2f91747
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2018-12-06 18:56:37 -1000

    complete checkAscOrder

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,26 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
+  // GOAL: numbers in each array are in ascending order.
+  // loop through array
+  // make sure that [i + 1] or [i - 1] is still an index in the array.
+  // if [i + 1] is greater than [i] then return true.
+  // create a variable to keep track of an array's ascending status.
+  // after the loop, detect any changes with the variable and return the correct value.
+  // alternative: to avoid if statements, just return the variable isCurrentlyAscending.
+
+        console.log ( 'TEST ' );
+        var isCurrentlyAscending = true;
+
+        for ( var i = 1; i < numArray.length; i++ ) {
+                if ( numArray[i] > numArray[i - 1] ) {
+
+                } else {
+                        isCurrentlyAscending = false;
+                        return false;
+
+                }
+
+        }
+        return isCurrentlyAscending;
+
 }
